d71d43bcb7d28c0559ac03cf96bbf334c2a52361
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-29 22:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+let userName = document.querySelector("#name");
+let age = document.querySelector("#age");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  onSubmit();
+});
+
+async function onSubmit() {
+  let ageVal = age.value;
+  let userNameVal = userName.value;
+
+  const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        ageVal > 18
+          ? alert(`Welcome, ${userNameVal}. You can vote.`)
+          : alert(`Oh sorry ${userNameVal}. You aren't old enough.`)
+      );
+    }, 4000);
+  });
+}
